9529aefec0295fbd1b1b13f36accb1c71704d8d4
OJIEHb <geiko2015@gmail.com>
2016-10-27 16:58:27 +0300

    fix some problems.

--- src/Main.java
+++ src/Main.java
@@ -5,57 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String [] args) {
         Scanner reader = new Scanner(System.in);
-        boolean end = false;
+        boolean continueToAskUserInput = false;
 
         System.out.println("Enter a number:");
 
-        while(!end){
+        while(!continueToAskUserInput){
             String number = reader.next();
-            if (number.startsWith("+")){
-                number = number.substring(1,number.length());
-            }
-            if(checkCountryCode(number)&&checkIsNumber(number)&&
-                    checkLength(number)&&checkOperatorCode(number)){
-                while(!(number.length()==1)){
-                    int sum=0;
-                    for(int i = 0; i < number.length();i++){
-                        sum += Integer.parseInt(""+number.charAt(i));
-                    }
-                    number = ""+sum;
-                }
-                switch (Integer.parseInt(number)){
-                    case 0:
-                        System.out.println("Zero");
-                        break;
-                    case 1:
-                        System.out.println("One");
-                        break;
-                    case 2:
-                        System.out.println("Two");
-                        break;
-                    case 3:
-                        System.out.println("Three");
-                        break;
-                    case 4:
-                        System.out.println("Four");
-                        break;
-                    case 5:
-                        System.out.println("Five");
-                        break;
-                    case 6:
-                        System.out.println("Six");
-                        break;
-                    case 7:
-                        System.out.println("Seven");
-                        break;
-                    case 8:
-                        System.out.println("Eight");
-                        break;
-                    case 9:
-                        System.out.println("Nine");
-                        break;
-                }
-                end = true;
+            number = deletePlus( number);
+            if(validateNumber(number)){
+                showResult(calculateSum(number));
+                continueToAskUserInput = true;
             }
         }
 
@@ -71,6 +30,13 @@ public class Main {
         }
     }
 
+    private  static String deletePlus(String s){
+        if (s.startsWith("+")){
+            return s.substring(1,s.length());
+        }else{
+            return s;
+        }
+    }
     private static boolean checkOperatorCode(String s){
         String [] operatorCodes = new String[]{"050","063","066","067","068","093","095","096","097","098"};
         for(String code:operatorCodes ){
@@ -99,9 +65,52 @@ public class Main {
         if(s.matches("[0-9]*")){
             System.out.println("It`s a number");
             return true;
-        }else{
-            System.out.println("It`s not a number: " + s);
-            return false;
+        }
+        System.out.println("It`s not a number: " + s);
+        return false;
+
+    }
+
+    private static boolean validateNumber(String s){
+        if(checkCountryCode(s)&&checkIsNumber(s)&&
+                checkLength(s)&&checkOperatorCode(s)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int calculateSum(String s) {
+        int sum=0;
+        while(!(s.length()==1)){
+            sum=0;
+            for(int i = 0; i < s.length();i++){
+                sum += Integer.parseInt(""+s.charAt(i));
+            }
+            s = ""+sum;
+        }
+        return sum;
+    }
+
+    private static void showResult(int i) {
+        switch (i){
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            default:
+                System.out.println(i);
+                break;
         }
     }
 
